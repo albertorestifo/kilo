819aefaf4cb7ace31aedf31a0b8995dc83101d41
Alberto Restifo <alberto.restifo@gmail.com>
2017-04-07 00:04:37 +0200

    Add movements

--- kilo.c
+++ kilo.c
@@ -10,16 +10,26 @@
 #include <unistd.h>
 
 /* }}} */
-
 /* Defines {{{ */
+
+#define KILO_VERSION "0.0.1"
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-/* }}} */
+enum editorKey {
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN,
+	PAGE_UP,
+	PAGE_DOWN,
+};
 
+/* }}} */
 /* Data {{{ */
 
 struct editorConfig {
+	int cx, cy;
 	int screenrows;
 	int screencols;
 	struct termios orig_termios;
@@ -28,7 +38,6 @@ struct editorConfig {
 struct editorConfig E;
 
 /* }}} */
-
 /* Terminal {{{ */
 
 void die(const char *s) {
@@ -59,13 +68,42 @@ void enableRawMode() {
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-char editorReadKey() {
+int editorReadKey() {
 	int nread;
 	char c;
 	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
 		if (nread == -1 && errno != EAGAIN) die("read");
 	}
-	return c;
+
+	if (c == '\x1b') {
+		char seq[3];
+
+		if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+		if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+		if (seq[0] == '[') {
+			if (seq[1] >= '0' && seq[1] <= '9') {
+				if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+				if (seq[2] == '~') {
+					switch (seq[1]) {
+						case '5': return PAGE_UP;
+						case '6': return PAGE_DOWN;
+					}
+				}
+			} else {
+				switch (seq[1]) {
+					case 'A': return ARROW_UP;
+					case 'B': return ARROW_DOWN;
+					case 'C': return ARROW_RIGHT;
+					case 'D': return ARROW_LEFT;
+				}
+			}
+		}
+
+		return '\x1b';
+	} else {
+		return c;
+	}
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -101,7 +139,6 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 /* }}} */
-
 /* Append buffer {{{ */
 
 struct abuf {
@@ -125,14 +162,28 @@ void abFree(struct abuf *ab) {
 }
 
 /* }}} */
-
 /* Output {{{ */
 
 void editorDrawRows(struct abuf *ab) {
 	int y;
 	for (y = 0; y < E.screenrows; y++) {
-		abAppend(ab, "~", 1);
+		if (y == E.screenrows / 3) {
+			char welcome[80];
+			int welcomelen = snprintf(welcome, sizeof(welcome),
+					"Kilo editor -- version %s", KILO_VERSION);
+			if (welcomelen > E.screencols) welcomelen = E.screencols;
+			int padding = (E.screencols - welcomelen) / 2;
+			if (padding) {
+				abAppend(ab, "~", 1);
+				padding--;
+			}
+			while (padding--) abAppend(ab, " ", 1);
+			abAppend(ab, welcome, welcomelen);
+		} else {
+			abAppend(ab, "~", 1);
+		}
 
+		abAppend(ab, "\x1b[K", 3);
 		if (y < E.screenrows -1) {
 			abAppend(ab, "\r\n", 2);
 		}
@@ -142,23 +193,51 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
 	struct abuf ab = ABUF_INIT;
 
-	abAppend(&ab, "\x1b[2J", 4);
+	abAppend(&ab, "\x1b[?25l", 6);
 	abAppend(&ab, "\x1b[H", 3);
 
 	editorDrawRows(&ab);
 
-	abAppend(&ab, "\x1b[H", 3);
+	char buf[32];
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+	abAppend(&ab, buf, strlen(buf));
+
+	abAppend(&ab, "\x1b[?25h", 6);
 
 	write(STDIN_FILENO, ab.b, ab.len);
 	abFree(&ab);
 }
 
 /* }}} */
-
 /* Input {{{ */
 
+void editorMoveCursor(int key) {
+	switch (key) {
+		case ARROW_LEFT:
+			if (E.cx != 0) {
+				E.cx--;
+			}
+			break;
+		case ARROW_DOWN:
+			if (E.cy != E.screenrows - 1) {
+				E.cy++;
+			}
+			break;
+		case ARROW_UP:
+			if (E.cy != 0) {
+				E.cy--;
+			}
+			break;
+		case ARROW_RIGHT:
+			if (E.cx != E.screencols - 1) {
+				E.cx++;
+			}
+			break;
+	}
+}
+
 void editorProcessKeypress() {
-	char c = editorReadKey();
+	int c = editorReadKey();
 
 	switch (c) {
 		case CTRL_KEY('q'):
@@ -166,14 +245,31 @@ void editorProcessKeypress() {
 			write(STDIN_FILENO, "\x1b[H", 3);
 			exit(0);
 			break;
+
+		case PAGE_UP:
+		case PAGE_DOWN:
+			{
+				int times = E.screenrows;
+				while (times--)
+					editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+			}
+
+		case ARROW_DOWN:
+		case ARROW_UP:
+		case ARROW_RIGHT:
+		case ARROW_LEFT:
+			editorMoveCursor(c);
+			break;
 	}
 }
 
 /* }}} */
-
 /* Init {{{ */
 
 void initEditor() {
+	E.cx = 0;
+	E.cy = 0;
+
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
